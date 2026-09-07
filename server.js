fe7d50bab4a48799647ee0b595e81cf7c94288d0
ksneoun-gif <ksneoun@gmail.com>
2026-09-07 21:36:36 +0900

    Add files via upload

--- server.js
+++ server.js
@@ -1,143 +1,114 @@
 const express = require('express');
 const http = require('http');
 const { Server } = require('socket.io');
-const path = require('path');
 
 const app = express();
 const server = http.createServer(app);
-
 const io = new Server(server, {
-    pingTimeout: 60000, 
-    pingInterval: 25000 
+    cors: { origin: "*" }
 });
 
-app.use(express.static(path.join(__dirname, 'public')));
+// HTML 파일이 있는 폴더를 정적 제공 (예: public 폴더에 index.html, admin.html 배치)
+app.use(express.static('public'));
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
-app.get('/admin', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'admin.html'));
-});
-
-const gameSessions = {}; 
+const sessions = {};
 
 io.on('connection', (socket) => {
+    console.log(`[+] 유저 접속: ${socket.id}`);
+
+    // 게임/관제 접속
     socket.on('join_game', (data) => {
-        const { sessionCode, name, rank, milId, isOperator } = data;
+        const { sessionCode, milId, name, rank, isOperator } = data;
         socket.join(sessionCode);
-        socket.sessionCode = sessionCode;
-        socket.isOperator = isOperator;
 
-        if (!gameSessions[sessionCode]) {
-            gameSessions[sessionCode] = { players: {} };
+        if (!sessions[sessionCode]) {
+            sessions[sessionCode] = { players: {}, turn: 1 };
         }
 
-        if (!isOperator) {
-            const existingId = Object.keys(gameSessions[sessionCode].players).find(
-                id => gameSessions[sessionCode].players[id].milId === milId
-            );
+        if (isOperator) {
+            socket.emit('update_dashboard', sessions[sessionCode].players);
+        } else {
+            sessions[sessionCode].players[socket.id] = {
+                socketId: socket.id, milId, name, rank,
+                balance: 50000000, trust: 10, debt: 0, execRate: 0,
+                turn: sessions[sessionCode].turn, status: '집행 대기중', lastAction: '접속 완료'
+            };
+            io.to(sessionCode).emit('update_dashboard', sessions[sessionCode].players);
+        }
+    });
 
-            if (existingId) {
-                const prev = gameSessions[sessionCode].players[existingId];
-                if (prev.status.includes('완료') || prev.status.includes('종료') || prev.status.includes('파산')) {
-                    gameSessions[sessionCode].players[socket.id] = {
-                        name, rank, milId, balance: 50000000, trust: 10, debt: 0,
-                        turn: 1, status: '의사결정 중', lastAction: '게임 시작'
-                    };
-                    if (existingId !== socket.id) delete gameSessions[sessionCode].players[existingId];
-                } else {
-                    gameSessions[sessionCode].players[socket.id] = prev;
-                    gameSessions[sessionCode].players[socket.id].status = prev.status.replace(' (네트워크 끊김)', '');
-                    if (existingId !== socket.id) delete gameSessions[sessionCode].players[existingId];
-                }
-            } else {
-                gameSessions[sessionCode].players[socket.id] = {
-                    name, rank, milId, balance: 50000000, trust: 10, debt: 0,
-                    turn: 1, status: '의사결정 중', lastAction: '게임 시작'
-                };
+    // 상태 업데이트
+    socket.on('update_state', (data) => {
+        let userSession = null;
+        for (const code in sessions) {
+            if (sessions[code].players[socket.id]) {
+                userSession = code;
+                break;
             }
         }
-        io.to(sessionCode).emit('update_dashboard', gameSessions[sessionCode].players);
-    });
-
-    socket.on('update_state', (stateData) => {
-        const sCode = socket.sessionCode;
-        if (sCode && gameSessions[sCode] && gameSessions[sCode].players[socket.id]) {
-            gameSessions[sCode].players[socket.id] = { ...gameSessions[sCode].players[socket.id], ...stateData };
-            io.to(sCode).emit('update_dashboard', gameSessions[sCode].players);
+        if (userSession) {
+            Object.assign(sessions[userSession].players[socket.id], data);
+            io.to(userSession).emit('update_dashboard', sessions[userSession].players);
         }
     });
 
+    // [신규] 운영자 개별 추경 예산 지급 기능
+    socket.on('admin_grant_budget', (data) => {
+        const { sessionCode, targetId, amount } = data;
+        if (sessions[sessionCode] && sessions[sessionCode].players[targetId]) {
+            io.to(targetId).emit('receive_special_budget', { amount });
+            console.log(`[!] ${sessionCode} 세션 - ${targetId}에게 추경 ${amount}원 배정`);
+        }
+    });
+
+    // 일괄 분기(턴) 진행
     socket.on('force_next_turn', (sessionCode) => {
-        if (gameSessions[sessionCode]) {
-            const players = gameSessions[sessionCode].players;
-            let canAdvance = false;
-
-            for (let id in players) {
-                if (!players[id].status.includes('파산') && !players[id].status.includes('종료')) {
-                    if (players[id].turn < 10) {
-                        canAdvance = true;
-                        players[id].turn += 1;
-                        players[id].status = '의사결정 중';
-                    } else {
-                        players[id].status = '게임 완료';
-                    }
-                }
+        if (sessions[sessionCode]) {
+            sessions[sessionCode].turn++;
+            for (let id in sessions[sessionCode].players) {
+                sessions[sessionCode].players[id].turn = sessions[sessionCode].turn;
+                sessions[sessionCode].players[id].status = '예산 집행 중';
             }
-
-            if (canAdvance) {
-                io.to(sessionCode).emit('trigger_next_turn');
-            } else {
-                io.to(sessionCode).emit('trigger_game_end');
-            }
-            io.to(sessionCode).emit('update_dashboard', players);
+            io.to(sessionCode).emit('trigger_next_turn');
+            io.to(sessionCode).emit('update_dashboard', sessions[sessionCode].players);
         }
     });
 
+    // 강제 결산 종료
     socket.on('force_end_game', (sessionCode) => {
-        if (gameSessions[sessionCode]) {
-            const players = gameSessions[sessionCode].players;
-            for (let id in players) {
-                if (!players[id].status.includes('파산')) {
-                    players[id].status = '게임 완료';
-                }
+        if (sessions[sessionCode]) {
+            for (let id in sessions[sessionCode].players) {
+                sessions[sessionCode].players[id].status = '종합 결산 완료';
             }
             io.to(sessionCode).emit('trigger_game_end');
-            io.to(sessionCode).emit('update_dashboard', players);
+            io.to(sessionCode).emit('update_dashboard', sessions[sessionCode].players);
         }
     });
 
-    socket.on('kick_player', ({ sessionCode, socketId }) => {
-        if (gameSessions[sessionCode] && gameSessions[sessionCode].players[socketId]) {
-            delete gameSessions[sessionCode].players[socketId];
+    // 개별 참가자 직위해제(추방)
+    socket.on('kick_player', (data) => {
+        const { sessionCode, socketId } = data;
+        if (sessions[sessionCode] && sessions[sessionCode].players[socketId]) {
+            delete sessions[sessionCode].players[socketId];
             io.to(socketId).emit('kicked');
-            io.to(sessionCode).emit('update_dashboard', gameSessions[sessionCode].players);
+            io.to(sessionCode).emit('update_dashboard', sessions[sessionCode].players);
         }
     });
 
+    // 세션 초기화
     socket.on('reset_session', (sessionCode) => {
-        if (gameSessions[sessionCode]) {
+        if (sessions[sessionCode]) {
             io.to(sessionCode).emit('session_reset');
-            gameSessions[sessionCode].players = {};
-            io.to(sessionCode).emit('update_dashboard', {});
+            delete sessions[sessionCode];
         }
     });
 
     socket.on('disconnect', () => {
-        const sCode = socket.sessionCode;
-        if (sCode && gameSessions[sCode] && !socket.isOperator) {
-            if (gameSessions[sCode].players[socket.id]) {
-                if (!gameSessions[sCode].players[socket.id].status.includes('끊김')) {
-                    gameSessions[sCode].players[socket.id].status += ' (네트워크 끊김)';
-                }
-                io.to(sCode).emit('update_dashboard', gameSessions[sCode].players);
-            }
-        }
+        console.log(`[-] 유저 연결 끊김: ${socket.id}`);
     });
 });
 
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
-    console.log(`🚀 서버 구동 완료 (Port: ${PORT})`);
+    console.log(`국방 재정 시뮬레이션 서버가 포트 ${PORT}에서 실행 중입니다.`);
 });
