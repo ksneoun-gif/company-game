5c15002b8bf223fd5c58ee1fb4858adc00a2d9ac
ksneoun-gif <ksneoun@gmail.com>
2026-09-08 23:04:35 +0900

    Add files via upload

--- server.js
+++ server.js
@@ -6,13 +6,11 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, { cors: { origin: "*" } });
 
-// 정적 파일 제공 (index.html, admin.html 등이 위치한 폴더)
 app.use(express.static('public'));
 
 const sessions = {};
 
 io.on('connection', (socket) => {
-    // 1. 관서(플레이어) 및 관제 접속
     socket.on('join_game', (data) => {
         const { sessionCode, milId, name, rank, isOperator } = data;
         socket.join(sessionCode);
@@ -24,18 +22,17 @@ io.on('connection', (socket) => {
         if (isOperator) {
             socket.emit('update_dashboard', sessions[sessionCode].players);
         } else {
+            // 💡 [수정] 가동률 50%, 리스크 20%로 초기화
             sessions[sessionCode].players[socket.id] = {
                 socketId: socket.id, milId, name, rank,
-                balance: 100000000, trust: 30, debt: 0, execRate: 0,
+                balance: 100000000, trust: 50, debt: 20, execRate: 0,
                 turn: sessions[sessionCode].turn, status: '집행 대기중', lastAction: '접속 완료'
             };
-            // 💡 [버그 2 해결] 접속 시 현재 서버 턴으로 동기화
             socket.emit('sync_turn', sessions[sessionCode].turn);
             io.to(sessionCode).emit('update_dashboard', sessions[sessionCode].players);
         }
     });
 
-    // 2. 상태 동기화
     socket.on('update_state', (data) => {
         let userSession = Object.keys(sessions).find(c => sessions[c].players[socket.id]);
         if (userSession) {
@@ -44,7 +41,6 @@ io.on('connection', (socket) => {
         }
     });
 
-    // 3. [신규] 운영자 개별 추경 지급
     socket.on('admin_grant_budget', (data) => {
         const { sessionCode, targetId, amount } = data;
         if (sessions[sessionCode] && sessions[sessionCode].players[targetId]) {
@@ -52,12 +48,10 @@ io.on('connection', (socket) => {
         }
     });
 
-    // 4. 일괄 분기 진행 (턴 강제 진행)
     socket.on('force_next_turn', (sessionCode) => {
         if (sessions[sessionCode]) {
             sessions[sessionCode].turn++;
             for (let id in sessions[sessionCode].players) {
-                // 💡 [버그 1 해결] 사고(게임오버) 처리된 유저는 턴을 진행시키지 않음
                 if (!sessions[sessionCode].players[id].status.includes('사고')) {
                     sessions[sessionCode].players[id].turn = sessions[sessionCode].turn;
                     sessions[sessionCode].players[id].status = '예산 집행 중';
@@ -68,7 +62,6 @@ io.on('connection', (socket) => {
         }
     });
 
-    // 5. 강제 결산 종료
     socket.on('force_end_game', (sessionCode) => {
         if (sessions[sessionCode]) {
             for (let id in sessions[sessionCode].players) {
@@ -79,7 +72,6 @@ io.on('connection', (socket) => {
         }
     });
 
-    // 6. 플레이어 추방
     socket.on('kick_player', (data) => {
         const { sessionCode, socketId } = data;
         if (sessions[sessionCode] && sessions[sessionCode].players[socketId]) {
@@ -89,7 +81,6 @@ io.on('connection', (socket) => {
         }
     });
 
-    // 7. 세션 초기화
     socket.on('reset_session', (sessionCode) => {
         if (sessions[sessionCode]) {
             io.to(sessionCode).emit('session_reset');
