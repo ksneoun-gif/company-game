6c275c87f679a4d910c7cab3fb30345e25dc23d9
ksneoun-gif <ksneoun@gmail.com>
2026-09-09 13:16:45 +0900

    Add files via upload

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const http = require('http');
 const { Server } = require('socket.io');
+const fs = require('fs');
 
 const app = express();
 const server = http.createServer(app);
@@ -10,20 +11,31 @@ app.use(express.static('public'));
 
 const sessions = {};
 
+// 💡 10턴 정상 마감 및 강제 종료 시 결과를 파일로 백업하는 공통 함수
+function saveSessionResult(sessionCode) {
+    if (!sessions[sessionCode]) return;
+    const fileName = `session_${sessionCode}_결산보고서_${Date.now()}.json`;
+    fs.writeFile(fileName, JSON.stringify(sessions[sessionCode], null, 2), 'utf8', (err) => {
+        if (err) {
+            console.error(`[-] 세션 ${sessionCode} 파일 저장 실패:`, err);
+        } else {
+            console.log(`[+] 세션 ${sessionCode} 결산 결과가 '${fileName}'에 안전하게 저장되었습니다.`);
+        }
+    });
+}
+
 io.on('connection', (socket) => {
     socket.on('join_game', (data) => {
         const { sessionCode, milId, name, rank, isOperator } = data;
         socket.join(sessionCode);
 
         if (!sessions[sessionCode]) {
-            sessions[sessionCode] = { players: {}, turn: 1, phase: 'playing' };
+            sessions[sessionCode] = { sessionCode, players: {}, turn: 1, phase: 'playing' };
         }
 
         if (isOperator) {
             socket.emit('update_dashboard', {
-                players: sessions[sessionCode].players,
-                turn: sessions[sessionCode].turn,
-                phase: sessions[sessionCode].phase
+                players: sessions[sessionCode].players, turn: sessions[sessionCode].turn, phase: sessions[sessionCode].phase
             });
         } else {
             sessions[sessionCode].players[socket.id] = {
@@ -33,14 +45,11 @@ io.on('connection', (socket) => {
             };
             socket.emit('sync_turn', sessions[sessionCode].turn);
             io.to(sessionCode).emit('update_dashboard', {
-                players: sessions[sessionCode].players,
-                turn: sessions[sessionCode].turn,
-                phase: sessions[sessionCode].phase
+                players: sessions[sessionCode].players, turn: sessions[sessionCode].turn, phase: sessions[sessionCode].phase
             });
         }
     });
 
-    // 💡 [버그 2 해결] 소켓 ID가 변경되더라도 고유 군번(milId)을 추적하여 실시간 연동 보장
     socket.on('update_state', (data) => {
         const { sessionCode, milId } = data;
         if (!sessionCode || !milId) return;
@@ -49,20 +58,17 @@ io.on('connection', (socket) => {
             let playerKey = Object.keys(sessions[sessionCode].players).find(k => sessions[sessionCode].players[k].milId === milId);
             
             if (playerKey) {
-                // 모바일 환경 등에서 소켓 연결이 끊겼다 다시 붙은 경우 식별자 강제 업데이트
                 if (playerKey !== socket.id) {
                     sessions[sessionCode].players[socket.id] = sessions[sessionCode].players[playerKey];
                     sessions[sessionCode].players[socket.id].socketId = socket.id;
                     delete sessions[sessionCode].players[playerKey];
                     playerKey = socket.id;
-                    socket.join(sessionCode); // 방 재입장
+                    socket.join(sessionCode);
                 }
                 
                 Object.assign(sessions[sessionCode].players[playerKey], data);
                 io.to(sessionCode).emit('update_dashboard', {
-                    players: sessions[sessionCode].players,
-                    turn: sessions[sessionCode].turn,
-                    phase: sessions[sessionCode].phase
+                    players: sessions[sessionCode].players, turn: sessions[sessionCode].turn, phase: sessions[sessionCode].phase
                 });
             }
         }
@@ -75,10 +81,24 @@ io.on('connection', (socket) => {
         }
     });
 
+    // 💡 10턴에서 다음 턴 진행 시 파일 저장 및 게임 종료 트리거 발동
     socket.on('force_next_turn', (sessionCode) => {
         if (sessions[sessionCode]) {
             sessions[sessionCode].turn++;
             let currentTurn = sessions[sessionCode].turn;
+
+            if (currentTurn > 10) {
+                for (let id in sessions[sessionCode].players) {
+                    sessions[sessionCode].players[id].status = '종합 결산 완료';
+                }
+                saveSessionResult(sessionCode); // 10턴 정상 완주 백업
+                io.to(sessionCode).emit('trigger_game_end');
+                io.to(sessionCode).emit('update_dashboard', {
+                    players: sessions[sessionCode].players, turn: 10, phase: 'ended'
+                });
+                return;
+            }
+
             let isSuppPhase = (currentTurn >= 4 && currentTurn <= 8);
             sessions[sessionCode].phase = isSuppPhase ? 'waiting_supp' : 'playing';
 
@@ -116,11 +136,13 @@ io.on('connection', (socket) => {
         }
     });
 
+    // 강제 종료 시에도 백업 진행
     socket.on('force_end_game', (sessionCode) => {
         if (sessions[sessionCode]) {
             for (let id in sessions[sessionCode].players) {
                 sessions[sessionCode].players[id].status = '종합 결산 완료';
             }
+            saveSessionResult(sessionCode); // 강제 종료 백업
             io.to(sessionCode).emit('trigger_game_end');
             io.to(sessionCode).emit('update_dashboard', {
                 players: sessions[sessionCode].players, turn: sessions[sessionCode].turn, phase: 'ended'
