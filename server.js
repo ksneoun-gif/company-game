5327e68faca7a4bbdaa07bed52528b26f3b92f03
ksneoun-gif <ksneoun@gmail.com>
2026-09-09 10:32:54 +0900

    Add files via upload

--- server.js
+++ server.js
@@ -16,20 +16,27 @@ io.on('connection', (socket) => {
         socket.join(sessionCode);
 
         if (!sessions[sessionCode]) {
-            sessions[sessionCode] = { players: {}, turn: 1 };
+            sessions[sessionCode] = { players: {}, turn: 1, phase: 'playing' };
         }
 
         if (isOperator) {
-            socket.emit('update_dashboard', sessions[sessionCode].players);
+            socket.emit('update_dashboard', {
+                players: sessions[sessionCode].players,
+                turn: sessions[sessionCode].turn,
+                phase: sessions[sessionCode].phase
+            });
         } else {
-            // 💡 [수정] 가동률 50%, 리스크 20%로 초기화
             sessions[sessionCode].players[socket.id] = {
                 socketId: socket.id, milId, name, rank,
-                balance: 100000000, trust: 50, debt: 20, execRate: 0,
+                balance: 75000000, trust: 45, debt: 30, execRate: 0, availableCash: 52000000,
                 turn: sessions[sessionCode].turn, status: '집행 대기중', lastAction: '접속 완료'
             };
             socket.emit('sync_turn', sessions[sessionCode].turn);
-            io.to(sessionCode).emit('update_dashboard', sessions[sessionCode].players);
+            io.to(sessionCode).emit('update_dashboard', {
+                players: sessions[sessionCode].players,
+                turn: sessions[sessionCode].turn,
+                phase: sessions[sessionCode].phase
+            });
         }
     });
 
@@ -37,7 +44,11 @@ io.on('connection', (socket) => {
         let userSession = Object.keys(sessions).find(c => sessions[c].players[socket.id]);
         if (userSession) {
             Object.assign(sessions[userSession].players[socket.id], data);
-            io.to(userSession).emit('update_dashboard', sessions[userSession].players);
+            io.to(userSession).emit('update_dashboard', {
+                players: sessions[userSession].players,
+                turn: sessions[userSession].turn,
+                phase: sessions[userSession].phase
+            });
         }
     });
 
@@ -48,17 +59,46 @@ io.on('connection', (socket) => {
         }
     });
 
+    // 💡 턴 넘김 시 4~8턴이면 '추경 대기' 상태로 전환
     socket.on('force_next_turn', (sessionCode) => {
         if (sessions[sessionCode]) {
             sessions[sessionCode].turn++;
+            let currentTurn = sessions[sessionCode].turn;
+            let isSuppPhase = (currentTurn >= 4 && currentTurn <= 8);
+            sessions[sessionCode].phase = isSuppPhase ? 'waiting_supp' : 'playing';
+
             for (let id in sessions[sessionCode].players) {
                 if (!sessions[sessionCode].players[id].status.includes('사고')) {
-                    sessions[sessionCode].players[id].turn = sessions[sessionCode].turn;
+                    sessions[sessionCode].players[id].turn = currentTurn;
+                    sessions[sessionCode].players[id].status = isSuppPhase ? '추경 검토 대기중' : '예산 집행 중';
+                }
+            }
+            
+            if (isSuppPhase) {
+                io.to(sessionCode).emit('wait_for_supp_budget', currentTurn);
+            } else {
+                io.to(sessionCode).emit('trigger_next_turn', currentTurn);
+            }
+            
+            io.to(sessionCode).emit('update_dashboard', {
+                players: sessions[sessionCode].players, turn: currentTurn, phase: sessions[sessionCode].phase
+            });
+        }
+    });
+
+    // 💡 운영자의 추경 완료 후 '본예산 집행 시작' 승인
+    socket.on('start_execution', (sessionCode) => {
+        if (sessions[sessionCode]) {
+            sessions[sessionCode].phase = 'playing';
+            for (let id in sessions[sessionCode].players) {
+                if (!sessions[sessionCode].players[id].status.includes('사고')) {
                     sessions[sessionCode].players[id].status = '예산 집행 중';
                 }
             }
             io.to(sessionCode).emit('trigger_next_turn', sessions[sessionCode].turn);
-            io.to(sessionCode).emit('update_dashboard', sessions[sessionCode].players);
+            io.to(sessionCode).emit('update_dashboard', {
+                players: sessions[sessionCode].players, turn: sessions[sessionCode].turn, phase: sessions[sessionCode].phase
+            });
         }
     });
 
@@ -68,7 +108,9 @@ io.on('connection', (socket) => {
                 sessions[sessionCode].players[id].status = '종합 결산 완료';
             }
             io.to(sessionCode).emit('trigger_game_end');
-            io.to(sessionCode).emit('update_dashboard', sessions[sessionCode].players);
+            io.to(sessionCode).emit('update_dashboard', {
+                players: sessions[sessionCode].players, turn: sessions[sessionCode].turn, phase: 'ended'
+            });
         }
     });
 
@@ -77,7 +119,9 @@ io.on('connection', (socket) => {
         if (sessions[sessionCode] && sessions[sessionCode].players[socketId]) {
             delete sessions[sessionCode].players[socketId];
             io.to(socketId).emit('kicked');
-            io.to(sessionCode).emit('update_dashboard', sessions[sessionCode].players);
+            io.to(sessionCode).emit('update_dashboard', {
+                players: sessions[sessionCode].players, turn: sessions[sessionCode].turn, phase: sessions[sessionCode].phase
+            });
         }
     });
 
